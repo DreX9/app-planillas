e74ecdaef017edf66f76e33ac72026ede8c26f0e
Frost173 <jordantrejo45@gmail.com>
2025-11-22 02:33:50 -0500

    Creacion de modal cargo

--- src/app/components/cargo/cargo.ts
+++ src/app/components/cargo/cargo.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-cargo',
-  imports: [],
+  imports: [MatTableModule, FormsModule, MatButtonModule, MatInputModule, MatIconModule],
   templateUrl: './cargo.html',
   styleUrl: './cargo.css',
 })
 export class Cargo {
+  busqueda = '';
+  //datos de la tabla
+  columns: string[] = [
+    'id',
+    'nombre',
+    'descripcion ',
+    'estado'
+  ];
+
+    dataCargo = new MatTableDataSource([
+    {
+      id: 1,
+      nombre: 'Gerente',
+      descripcion: 'Encargado de la gestion general de la empresa',
+      estado: '21/11/2025'
+    },
+    {
+      id: 2,
+      nombre: 'Jefe Supervisor',
+      descripcion: 'Encargado de supervisar al personal de la empresa',
+      estado: '21/11/2025'
+    }
+  ]);
+
+  dataOriginal = [...this.dataCargo.data]; // para el buscador
 
 }
---
+++ src/app/components/modales/modal-cargo/modal-cargo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-cargo',
+  imports: [],
+  templateUrl: './modal-cargo.html',
+  styleUrl: './modal-cargo.css',
+})
+export class ModalCargo {
+
+}
